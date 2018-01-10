30201ada380b02c0e83cf89fdb65a52041bc1bde
adityachatterjee <adityachatterjee42@gmail.com>
2018-01-10 15:57:37 -0800

    modified admin console

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
+class ChoiceInline(admin.StackedInline):
+	model  = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fields = ['pub_date', 'question_text']
+	inlines = [ChoiceInline]
+	
 admin.site.register(Question)
 
